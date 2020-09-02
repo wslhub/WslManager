b167a44fac2f149fd24459f293501c17f89f86d6
Jung Hyun Nam <rkttu@rkttu.com>
2020-09-02 15:38:12 +0900

    Add model and install form

---
+++ src/WslManager/ViewModels/InstallSourceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WslManager.ViewModels
+{
+    public sealed class InstallSourceModel
+    {
+        // https://wslhub.com/WslManager/data/install-source.json
+
+        public List<MicrosoftStoreModel> MicrosoftStoreUrls { get; set; }
+            = new List<MicrosoftStoreModel>();
+
+        public List<AppxSideloadModel> AppxSideload { get; set; }
+            = new List<AppxSideloadModel>();
+
+        public List<RootFsModel> RootFs { get; set; }
+            = new List<RootFsModel>();
+    }
+}
---
+++ src/WslManager/ViewModels/MicrosoftStoreModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WslManager.ViewModels
+{
+    public sealed class MicrosoftStoreModel : NotifiableModel
+    {
+        private string _displayName;
+        private string _providerName;
+        private string _url;
+
+        public string DisplayName
+        {
+            get => _displayName;
+            set
+            {
+                if (value != _displayName)
+                {
+                    _displayName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string ProviderName
+        {
+            get => _providerName;
+            set
+            {
+                if (value != _providerName)
+                {
+                    _providerName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Url
+        {
+            get => _url;
+            set
+            {
+                if (value != _url)
+                {
+                    _url = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
